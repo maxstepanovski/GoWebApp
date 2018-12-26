1f2f6c706a7f74724cf339acaa7dcdf72a259aed
maksim stepanov <stepanov.m.sergeevi@sberbank.ru>
2018-12-26 11:13:23 +0300

    added api point to read objects from db

--- main/secondChapter.go
+++ main/secondChapter.go
@@ -12,15 +12,26 @@ import (
 	"strconv"
 )
 
+var DB_NAME = "mysql"
+var DB_CREDENTIALS = "root:i86Kwp5a@tcp(127.0.0.1:3306)/new_schema"
+var CREATE_ENDPOINT = "/api/user/create"
+var READ_ENDPOINT = "/api/user/read"
+var LOCAL_HOST = ":8080"
+
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/api/user/create", CreateUser)
+	router.HandleFunc(CREATE_ENDPOINT, CreateUser)
+	router.HandleFunc(READ_ENDPOINT, ReadUser)
 	http.Handle("/", router)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(LOCAL_HOST, nil)
 }
 
+/**
+reading query parameters, creating User object and writing it to mySql database
+*/
 func CreateUser(responseWriter http.ResponseWriter, request *http.Request) {
 	User := s.User{}
+	User.ID, _ = strconv.Atoi(request.FormValue("id"))
 	User.Name = request.FormValue("name")
 	User.Email = request.FormValue("email")
 	User.First = request.FormValue("first")
@@ -30,16 +41,43 @@ func CreateUser(responseWriter http.ResponseWriter, request *http.Request) {
 		fmt.Println("marshalling error!")
 	}
 
-	database, error := sql.Open("mysql", "root:i86Kwp5a@tcp(127.0.0.1:3306)/new_schema")
+	database, error := sql.Open(DB_NAME, DB_CREDENTIALS)
 	defer database.Close()
 
 	if error != nil {
 		fmt.Println("couldn't open db")
 	}
 
-	result, err := database.Exec("INSERT INTO users set ID='" + strconv.Itoa(User.ID) + "', Nickname='" + User.Name + "', First='" + User.First + "', Last='" + User.Last + "', Email='" + User.Email + "'")
+	_, err := database.Exec(
+		"INSERT INTO users set ID='" + strconv.Itoa(User.ID) +
+			"', Nickname='" + User.Name +
+			"', First='" + User.First +
+			"', Last='" + User.Last +
+			"', Email='" + User.Email +
+			"'")
 	if err != nil {
 		fmt.Fprintf(responseWriter, err.Error())
 	}
-	fmt.Println(result)
+	fmt.Fprintf(responseWriter, "object written to database")
+	fmt.Fprintf(responseWriter, User.ToString())
+}
+
+/**
+Read a user from sql database via webapplication api
+*/
+func ReadUser(responseWriter http.ResponseWriter, request *http.Request) {
+	id := request.FormValue("id")
+	ReadUser := s.User{}
+
+	database, error := sql.Open(DB_NAME, DB_CREDENTIALS)
+	defer database.Close()
+	if error != nil {
+		fmt.Println("couldn't open db")
+	}
+
+	err := database.QueryRow("select * from users where ID=?", id).Scan(&ReadUser.ID, &ReadUser.Name, &ReadUser.First, &ReadUser.Last, &ReadUser.Email)
+	if err != nil {
+		fmt.Fprintf(responseWriter, err.Error())
+	}
+	fmt.Fprintf(responseWriter, ReadUser.ToString())
 }
--- main/secondary/user.go
+++ main/secondary/user.go
@@ -1,9 +1,15 @@
 package secondary
 
+import "strconv"
+
 type User struct {
-	ID int "json:id"
-	Name string "json:name"
+	ID    int    "json:id"
+	Name  string "json:name"
 	Email string "json:email"
 	First string "json:email"
-	Last string "json:email"
+	Last  string "json:email"
+}
+
+func (u *User) ToString() string {
+	return strconv.Itoa(u.ID) + " " + u.Name + " " + u.Email + " " + u.First + " " + u.Last
 }
